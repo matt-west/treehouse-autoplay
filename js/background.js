9c540c852bb69d15d08008904b0bc07ee2dcd787
Matt West <mattantwest@gmail.com>
2013-10-29 09:02:01 +0000

    Cleaning up file structure

--- js/background.js
+++ js/background.js
@@ -2,11 +2,11 @@ $(function() {
   chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
       if (request.active == "true") {
-        chrome.browserAction.setIcon({"path": "active.png"});
+        chrome.browserAction.setIcon({"path": "../img/active.png"});
         fs = localStorage["fullscreen"];
         sendResponse({fullscreen: fs});
       } else if(request.active == "false") {
-        chrome.browserAction.setIcon({"path": "inactive.png"});
+        chrome.browserAction.setIcon({"path": "../img/inactive.png"});
         sendResponse({});
       } else {
         sendResponse({});
